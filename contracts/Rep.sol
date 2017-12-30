cdb009ce0ee5234453c6b01dc20e509ae0258e2c
mzh <ma.hed@bluewin.ch>
2017-12-30 19:01:21 +0100

    Contract

---
+++ contracts/Rep.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.17;
+
+//import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+// contract TutorialToken is StandardToken {
+
+contract Rep {
+
+    string public name = "Rep";
+    string public symbol = "REP";
+    uint8 public decimals = 0;
+    uint public INITIAL_SUPPLY = 10000;
+
+
+    function Rep() public {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
+
+}
---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+App = {
+    web3Provider: null,
+    contracts:    {},
+
+    init: function() {},
+
+    initWeb3: function() {},
+
+    initContract: function() {},
+
+    bindEvents: function() {},
+
+    // Transfer, Transaction, ..., see ERC20Basic
+    handleX: function(event) {},
+}
