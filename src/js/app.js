332cdcfdb94578b9e63a7f207cb78fba5a1d0b77
mzh <ma.hed@bluewin.ch>
2017-12-30 21:11:43 +0100

    Web3

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,42 @@
 App = {
     web3Provider: null,
+
+    init: function() {
+        App.initWeb3();
+    },
+
+    initWeb3: function() {
+        var host = "http://localhost:8545";
+        App.web3Provider = new Web3.providers.HttpProvider(
+                                   host);
+        web3 = new Web3(App.web3Provider);
+
+        return App.bindEvents();
+    },
+
+    bindEvents: function() {
+        $(document).on("click", ".btn", App.listAccounts);
+    },
+
+    listAccounts: function(event) {
+        event.preventDefault();
+        accounts = web3.eth.accounts;
+        $('#RPCaccounts').html(accounts);
+    }
+
+    /*
     contracts:    {},
 
-    init: function() {},
-
-    initWeb3: function() {},
 
     initContract: function() {},
 
-    bindEvents: function() {},
-
     // Transfer, Transaction, ..., see ERC20Basic
     handleX: function(event) {},
-}
+
+    getBalances: function() {},
+    */
+};
+
+$(function() {
+    $(window).load(function() {App.init(); })
+});
